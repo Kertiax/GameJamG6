d22d0b762255c8fe8266b6a091536ee14aafff22
Kertiax <160547654+Kertiax@users.noreply.github.com>
2024-03-08 10:50:35 -0500

    Update scene manager

---
+++ Assets/SceneManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManager : MonoBehaviour
+{
+    public string nextSceneName;
+
+    private void Start()
+    {
+        LoadNextScene(nextSceneName);
+    }
+
+    
+
+   public void LoadNextScene( string nextSceneName)
+    {
+        
+    }
+   
+
+  
+}
