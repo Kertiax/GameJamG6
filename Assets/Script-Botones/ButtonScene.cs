1b3d07a8997a93262916e383997800c4e18c18dc
sara-34 <93350930+sara-34@users.noreply.github.com>
2024-03-07 21:57:27 -0500

    Subiendo Cambios UX

--- Assets/Script-Botones/ButtonScene.cs
+++ Assets/Script-Botones/ButtonScene.cs
@@ -7,8 +7,36 @@ using UnityEngine;
 
 public class ButtonScene : MonoBehaviour
 {
+    private bool isPause;
+    [SerializeField] GameObject panelPause;
+
+
     public void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
     }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            isPause = !isPause;
+            PauseGame();
+        }
+    }
+
+    public void PauseGame()
+    {
+        if (isPause)
+        {
+            Time.timeScale = 0;
+            panelPause.SetActive(true);
+        }
+        else
+        {
+            Time.timeScale = 1;
+            panelPause.SetActive(false);
+        }
+    }
+
 }
